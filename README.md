b51eb28a59c3c98d1ae8c685f10ee6931dfb36a1
brolag <brolag@live.com>
2019-05-01 18:49:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-costa-rica
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-costa-rica)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  img = 'https://pbs.twimg.com/profile_images/1096540294550478848/ar_1Hq7C_400x400.png';
+}
